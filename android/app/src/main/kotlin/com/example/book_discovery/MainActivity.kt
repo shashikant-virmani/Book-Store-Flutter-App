d6d15f974293d089f0885e81a4feaadc5a9e5d2c
Shashikant <kants52167@gmail.com>
2025-03-20 18:40:45 +0530

    app bar done

---
+++ android/app/src/main/kotlin/com/example/book_discovery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.book_discovery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Book   Store',
+        style: TextStyle(color: Colors.purple, fontSize: 34, fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.lightGreenAccent,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 239, 138, 255),
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: SvgPicture.asset('assets/icons/arrow_back.svg', height: 20, width: 20,),
+        ),
+        actions: [
+          GestureDetector(
+          child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          width: 37,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 239, 138, 255),
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: SvgPicture.asset('assets/icons/two-dots.svg', height: 20, width: 20,),
+        ),
+          ),
+        ],
+      ),
+    );
+  }
+}
